7f567d75cb1789229b523579149cf8778c0caa90
osqq <osqq95@gmail.com>
2019-12-19 10:15:43 +0200

    incomplete history search bundle

---
+++ bundles/seutumaisa-history-search/resources/locale/sv.js
@@ -0,0 +1,66 @@
+Oskari.registerLocalization({
+    "lang": "sv",
+    "key": "seutumaisa-search",
+    "value": {
+        "title" : "Maamassan historiahaku",
+        "desc" : "",
+        "tile": {
+            "title": "Maamassan historiahaku"
+        },
+        "flyout": {
+            "title": "Maamassan historiahaku",
+            "searchtab": {
+                "title": "Haku",
+                "noValues": "(arvoja ei saatavilla)",
+                "search": "Hae",
+                "clear": "Tyhjennä"
+            },
+            "resulttab": {
+                "error": {
+                    "title": "Virhe",
+                    "message": "Historiahakua ei voitu suorittaa juuri nyt, kokeile hetken päästä uudelleen"
+                },
+                "noresults": {
+                    "title": "Ei hakutuloksia",
+                    "message": "Ei hakutuloksia, kokeile erilaisella haulla"
+                },
+                "datatable": {
+                  "sEmptyTable": "Tabellen innehåller ingen data",
+                  "sInfo": "Visar _START_ till _END_ av totalt _TOTAL_ rader",
+                  "sInfoEmpty": "Visar 0 till 0 av totalt 0 rader",
+                  "sInfoFiltered": "(filtrerade från totalt _MAX_ rader)",
+                  "sInfoPostFix": "",
+                  "sInfoThousands": " ",
+                  "sLengthMenu": "Visa _MENU_ rader",
+                  "sLoadingRecords": "Laddar...",
+                  "sProcessing": "Bearbetar...",
+                  "sSearch": "Sök:",
+                  "sZeroRecords": "Hittade inga matchande resultat",
+                  "oPaginate": {
+                    "sFirst": "Första",
+                    "sLast": "Sista",
+                    "sNext": "Nästa",
+                    "sPrevious": "Föregående"
+                  },
+                  "oAria": {
+                    "sSortAscending": ": aktivera för att sortera kolumnen i stigande ordning",
+                    "sSortDescending": ": aktivera för att sortera kolumnen i fallande ordning"
+                  },
+                  "buttons": {
+                        "copy": "Copy",
+                        "csv": "CSV",
+                        "print": "Print"
+                    },
+                    "select": {
+                        "rows": {
+                            _: "%d riviä valittu",
+                            0: "Klikkaa riviä valitaksesi se",
+                            1: "Yksi rivi valittu"
+                        }
+                    }
+                }
+            }
+        },
+        "searchTitle": "Osoitehaku"
+    }
+});
