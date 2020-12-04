2dd96425549e2ce49a26cb9a412b7f625aca3578
Jani Levonen <jani.levonen@sitowise.com>
2020-12-04 15:48:10 +0200

    Fix and edit locales

--- bundles/hsy-lang-overrides/resources/locale/fi.js
+++ bundles/hsy-lang-overrides/resources/locale/fi.js
@@ -1,166 +1,5 @@
 Oskari.registerLocalization({
     "lang": "fi",
-    "key": "Printout",
-    "value": {
-        "title": "Tulosta näkymä",
-        "flyouttitle": "Siirry tulostamaan",
-        "desc": "",
-        "btnTooltip": "Tulosta",
-        "BasicView": {
-            "title": "Tulosta nykyinen karttanäkymä. (Tulosteessa voi olla esitettynä kerrallaan vain 8 karttatasoa.)",
-            "name": {
-                "label": "Kartan nimi",
-                "placeholder": "pakollinen",
-                "tooltip": "Anna näkymälle kuvaileva nimi. Huomioi käyttöliittymän kieli."
-            },
-            "language": {
-                "label": "Kieli",
-                "options": {
-                    "fi": "Suomi",
-                    "sv": "Ruotsi",
-                    "en": "Englanti"
-                },
-                "tooltip": "Valitse kieli, jota käytetään tulosteella. Huomioi käyttöliittymän ja aineiston kieli."
-            },
-            "size": {
-                "label": "Koko",
-                "tooltip": "Valitse arkkikoko. Näet karttanäkymän valitussa koossa esikatselukuvassa.",
-                "options": [{
-                    "id": "A4",
-                    "label": "A4-tuloste",
-                    "classForPreview": "preview-portrait",
-                    "selected": true
-                }, {
-                    "id": "A4_Landscape",
-                    "label": "A4-vaakatuloste",
-                    "classForPreview": "preview-landscape"
-                }, {
-                    "id": "A3",
-                    "label": "A3-tuloste",
-                    "classForPreview": "preview-portrait"
-                }, {
-                    "id": "A3_Landscape",
-                    "label": "A3-vaakatuloste",
-                    "classForPreview": "preview-landscape"
-                }]
-            },
-            "preview": {
-                "label": "Esikatselu",
-                "tooltip": "Voit suurentaa esikatselukuvaa klikkaamalla kuvaa hiirellä.",
-                "pending": "Esikatselukuva päivitetään hetken kuluttua.",
-                "notes": {
-                    "extent": "Esikatselukuvasta voit tarkistaa tulosteen kattavuusalueen, joka poikkeaa hieman karttaikkunalla esitetystä alueesta.",
-                    "restriction": "Esikatselukuvassa ei näytetä kaikkia karttatasoja."
-                }
-            },
-            "buttons": {
-                "save": "Hae tuloste",
-                "ok": "OK",
-                "back": "Edellinen",
-                "cancel": "Peruuta"
-            },
-            "location": {
-                "label": "Sijainti ja mittakaavataso",
-                "tooltip": "Mittakaavataso vastaa selaimessa näkyvän karttanäkymän mittakaavaa.",
-                "zoomlevel": "Mittakaavataso"
-            },
-            "settings": {
-                "label": "Lisäasetukset",
-                "tooltip": "Valitse karttatulosteelle tiedostomuoto, otsikko, mittakaava ja päivämäärä."
-            },
-            "format": {
-                "label": "Tiedostomuoto",
-                "tooltip": "Valitse tulosteen tiedostomuoto",
-                "options": [{
-                    "id": "png",
-                    "format": "image/png",
-                    "label": "PNG-kuva"
-                }, {
-                    "id": "pdf",
-                    "format": "application/pdf",
-                    "selected": true,
-                    "label": "PDF-dokumentti"
-                }]
-            },
-            "mapTitle": {
-                "label": "Pdf-dokumenttiin lisättävät ominaisuudet: </br>Kartan otsikko",
-                "tooltip": "Anna karttatulosteelle otsikko."
-            },
-            "content": {
-                "options": [{
-                    "id": "pageLogo",
-                    "label": "Näytä HSYn logo tulosteessa.",
-                    "tooltip": "Voit halutessasi piilottaa HSYn logon tulosteesta.",
-                    "checked": "checked"
-                }, {
-                    "id": "pageScale",
-                    "label": "Näytä mittakaava",
-                    "tooltip": "Voit halutessasi näyttää tulosteessa kartan mittakaavan.",
-                    "checked": "checked"
-                }, {
-                    "id": "pageDate",
-                    "label": "Näytä päivämäärä",
-                    "tooltip": "Voit halutessasi lisätä tulosteeseen päivämäärän.",
-                    "checked": "checked"
-                }]
-            },
-            "legend": {
-                "label": "Karttaselite",
-                "tooltip": "Valitse karttaselitteelle paikka. Karttaselitettä ei näytetä tulosteessa, jos paikkaa ei valittuna.",
-                "options": [{
-                    "id": "oskari_legend_NO",
-                    "loca": "NO",
-                    "label": "Ei karttaselitettä.",
-                    "tooltip": "Karttaselitettä ei näytetä tulosteessa.",
-                    "selected": true
-                }, {
-                    "id": "oskari_legend_LL",
-                    "loca": "LL",
-                    "label": "Vasen alanurkka",
-                    "tooltip": "Karttaselite näytetään tulosteen vasemmassa alanurkassa."
-                }, {
-                    "id": "oskari_legend_LU",
-                    "loca": "LU",
-                    "label": "Vasen ylänurkka",
-                    "tooltip": "Karttaselite näytetään tulosteen vasemmassa ylänurkassa."
-                }, {
-                    "id": "oskari_legend_RU",
-                    "loca": "RU",
-                    "label": "Oikea ylänurkka",
-                    "tooltip": "Karttaselite näytetään tulosteen oikeassa ylänurkassa."
-                }, {
-                    "id": "oskari_legend_RL",
-                    "loca": "RL",
-                    "label": "Oikea alanurkka",
-                    "tooltip": "Karttaselite näytetään tulosteen oikeassa alanurkassa."
-                }]
-            },
-            "help": "Ohje",
-            "error": {
-                "title": "Virhe",
-                "size": "Tulosteen koko on virheellinen.",
-                "name": "Kartan nimi on pakollinen tieto.",
-                "nohelp": "Ohjetta ei löytynyt.",
-                "saveFailed": "Karttanäkymän tulostus epäonnistui. Yritä myöhemmin uudelleen.",
-                "nameIllegalCharacters": "Kartan nimessä on luvattomia merkkejä. Sallittuja merkkejä ovat kaikki suomen kielen aakkoset, numerot sekä välilyönti ja yhdysmerkki."
-            }
-        },
-        "StartView": {
-            "text": "Voit tulostaa tekemäsi karttanäkymän PNG-kuvaksi tai PDF-tulosteeksi.",
-            "info": {
-                "maxLayers": "Tulosteessa voi olla enintään kahdeksan karttatasoa.",
-                "printoutProcessingTime": "Kartan tulostus voi kestää hetken, jos useita karttatasoja on valittuna."
-            },
-            "buttons": {
-                "continue": "Jatka",
-                "cancel": "Peruuta"
-            }
-        }
-    }
-}, true);
-
-Oskari.registerLocalization({
-    "lang": "fi",
     "key": "userinterface.UserGuide",
     "value": {
         "tabs": [{
@@ -306,3 +145,126 @@ Oskari.registerLocalization({
     }
 }
 ,true);
+
+Oskari.registerLocalization({
+    "lang": "fi",
+    "key": "Printout",
+    "value": {
+        "title": "Tulostus",
+        "flyouttitle": "Tulostus",
+        "desc": "",
+        "btnTooltip": "Tulosta nykyinen karttanäkymä png-kuvaksi tai pdf-tiedostoon.",
+        "BasicView": {
+            "title": "Tulosta karttanäkymä",
+            "name": {
+                "label": "Kartan nimi",
+                "placeholder": "pakollinen",
+                "tooltip": "Anna tulostettavalle kartalle nimi. Ota huomioon kartan teksteissä käytetty kieli."
+            },
+            "language": {
+                "label": "Kieli",
+                "options": {
+                    "fi": "suomi",
+                    "sv": "ruotsi",
+                    "en": "englanti"
+                },
+                "tooltip": "Valitse tulosteessa käytettävä kieli. Ota huomioon käyttöliittymässä ja aineistossa käytetty kieli."
+            },
+            "size": {
+                "label": "Koko ja suunta",
+                "tooltip": "Valitse tulosteen arkkikoko ja suunta. Tarkista muutokset esikatselukuvasta.",
+                "options": {
+                    "A4": "A4 pystysuunta",
+                    "A4_Landscape": "A4 vaakasuunta",
+                    "A3": "A3 pystysuunta",
+                    "A3_Landscape": "A3 vaakasuunta"
+                }
+            },
+            "preview": {
+                "label": "Esikatselu",
+                "pending": "Esikatselukuva päivitetään hetken kuluttua.",
+                "notes": {
+                    "extent": "Tarkista tulosteen kattavuusalue esikatselukuvasta."
+                }
+            },
+            "buttons": {
+                "save": "Tulosta",
+                "ok": "OK",
+                "back": "Edellinen",
+                "cancel": "Peruuta"
+            },
+            "location": {
+                "label": "Sijainti ja mittakaava",
+                "tooltip": "Kartan mittakaava tulosteessa vastaa esikatselukuvassa näytettävän kartan mittakaavaa.",
+                "zoomlevel": "Mittakaava"
+            },
+            "settings": {
+                "label": "Lisäasetukset",
+                "tooltip": "Valitse asetukset karttatulosteelle."
+            },
+            "format": {
+                "label": "Tiedostomuoto",
+                "tooltip": "Valitse tiedostomuoto, jossa haluat tulosteen.",
+                "options": {
+                    "png": "PNG-kuva",
+                    "pdf": "PDF-dokumentti"
+                }
+            },
+            "content": {
+                "label": "Näytettävät tiedot",
+                "tooltip": "Valitse tulosteessa näytettävät tiedot.",
+                "pngNote": "PNG-tulosteelle ei lisätä alla olevia tietoja.",
+                "mapTitle": {
+                    "placeholder": "Otsikko"
+                },
+                "pageLogo": {
+                    "label": "Näytä palvelun logo",
+                    "tooltip": "Näytä tulosteessa tämän palvelun logo."
+                },
+                "pageScale": {
+                    "label": "Näytä mittakaava",
+                    "tooltip": "Näytä tulosteessa kartan mittakaava."
+                },
+                "pageDate": {
+                    "label": "Näytä päivämäärä",
+                    "tooltip": "Näytä tulosteessa sen laatimispäivämäärä."
+                },
+                "pageTimeSeriesTime": {
+                    "label": "Näytä aikasarjan ajanhetki",
+                    "tooltip": "Näytä tulosteessa aikasarjan ajanhetki.",
+                    "printLabel": "Aikasarjan ajanhetki"
+                }
+            },
+            "help": "Ohje",
+            "error": {
+                "title": "Virhe",
+                "size": "Tulosteen koko on virheellinen.",
+                "name": "Kartalle ei ole annettu nimeä. Anna kartan nimi ja yritä uudelleen.",
+                "nohelp": "Ohjetta ei löytynyt.",
+                "saveFailed": "Tulostus epäonnistui.",
+                "nameIllegalCharacters": "Kartan nimessä on kiellettyjä merkkejä. Sallittuja merkkejä ovat suomen kielen aakkoset (a-ö, A-Ö), numerot (0-9), välilyönti ja yhdysmerkki (-)."
+            },
+            "scale": {
+                "label": "Mittakaava",
+                "tooltip": "Määritä tulostuksessa käytettävä mittakaava",
+                "map": "Käytä kartan mittakaavaa",
+                "configured": "Valitse mittakaava",
+                "unsupportedLayersMessage": "Seuraavat tasot eivät tulostu mittakaava valinnalla",
+                "unsupportedLayersTitle": "Tulosteessa ei näy kaikki tasot"
+            }
+        },
+        "StartView": {
+            "text": "Tulosta karttanäkymä tiedostoon. Tiedosto voi olla joko PNG-kuva tai PDF-tiedosto.",
+            "info": {
+                "maxLayers": "Tulosteessa voi olla enintään kahdeksan karttatasoa.",
+                "printoutProcessingTime": "Tulostus voi kestää hetken, jos valittuna on useita karttatasoja."
+            },
+            "buttons": {
+                "continue": "Jatka",
+                "cancel": "Peruuta"
+            }
+        }
+    }
+}
+, true);
+    
