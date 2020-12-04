a7d869023cf03c41776a8a5faaa6ad5ed3a706de
Jani Levonen <jani.levonen@sitowise.com>
2020-12-04 13:07:23 +0200

    Add lang override for Search-bundle

--- bundles/hsy-lang-overrides/resources/locale/fi.js
+++ bundles/hsy-lang-overrides/resources/locale/fi.js
@@ -260,4 +260,49 @@ Oskari.registerLocalization({
             }
         }
     }
-},true);
+}
+,true);
+
+Oskari.registerLocalization({
+    "lang": "fi",
+    "key": "Search",
+    "value": {
+        "title": "Haku",
+        "desc": "",
+        "tabTitle": "Paikkahaku",
+        "invalid_characters": "Hakusanassa on kiellettyjä merkkejä. Sallittuja merkkejä ovat aakkoset (a-ö, A-Ö), numerot (0-9) sekä piste (.), pilkku (,), yhdysviiva (-) ja huutomerkki (!). Voit myös korvata sanassa yhden merkin kysymysmerkillä (?) tai sana loppuosan jokerimerkillä (*).",
+        "searchDescription": "Hae paikkoja paikannimen perusteella.",
+        "searchAssistance": "Anna hakusana",
+        "searchResultCount": "Hakusanalla löytyi",
+        "searchResultCount2": "hakutulosta.",
+        "searchResultDescriptionMoreResults": "Rajaa hakutulosten määrää tarkentamalla hakusanaa.",
+        "searchResultDescriptionOrdering": "Järjestä hakutulokset klikkaamalla sarakkeen otsikkoa alla olevassa taulukossa.",
+        "searchResults": "Hakutulokset:",
+        "searchResultsDescription": "hakutulosta hakusanalla",
+        "searchservice_search_alert_title": "Virhe",
+        "searchservice_search_not_found_anything_text": "Hakusanalla ei löytynyt yhtään hakutulosta. Tarkista hakusanan oikeinkirjoitus.",
+        "too_short": "Hakusana on liian lyhyt. Hakusanassa on oltava vähintään yksi merkki. Jos käytät jokerimerkkiä (*), hakusanassa on oltava vähintään neljä muuta merkkiä.",
+        "cannot_be_empty": "Hakusanassa on oltava vähintään yksi merkki.",
+        "too_many_stars": "Hakusanassa saa olla enintään yksi jokerimerkki (*) sanan lopussa.",
+        "generic_error": "Haku epäonnistui.",
+        "grid": {
+            "name": "Nimi",
+            "village": "Kunta",
+            "region": "Alue",
+            "type": "Tyyppi"
+        },
+        "resultBox": {
+            "close": "Sulje",
+            "title": "Hakutulokset",
+            "alternatives": "Tällä paikalla on seuraavia vaihtoehtoisia nimiä:"
+        },
+        "guidedTour": {
+            "title": "Haku",
+            "message": "Haku-valikossa voit hakea paikkoja tai paikkatietoja. <br/><br/>  Paikkahaku: Hae paikkoja paikannimen tai osoitteen perusteella. Klikkaa hakutulosta ja kartta keskittyy valittuun paikkaan. <br/><br/> Metatietohaku: Hae paikkatietoaineistoja, -aineistosarjoja ja -palveluja. Käytä tekstihakua tai valmiiksi määriteltyjä hakuehtoja. Hakutuloksista pääset lukemaan metatietokuvailun tai avaamaan valitun aineiston kartalla, jos karttataso on saatavilla.",
+            "openLink": "Näytä Haku",
+            "closeLink": "Piilota Haku",
+            "tileText": "Haku"
+        }
+    }
+}
+,true);
